95ac865a01891d37f11b389a7e5fc43dcc984098
sumeshkv11 <124475534+sumeshkv11@users.noreply.github.com>
2023-05-11 19:35:54 -0500

    Uploaded my TravelItenerary app

---
+++ TravelItenerary/TravelItenerary/TravelIteneraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelIteneraryApp.swift
+//  TravelItenerary
+//
+//  Created by Sumesh Kesavamoorthy Vijayalakshmi on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelIteneraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
